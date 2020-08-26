5b505b0bc1f1b6860f28afb8a0bbd4d0beaeb515
slimane rechdi <slimane.rechdi@gmail.com>
2020-08-26 11:22:30 -0400

    add queries

--- __main__.py
+++ __main__.py
@@ -1,45 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-Query in SolidEdge:
-==================
-
-/// QueryScopeConstants Enumeration
-
-seQueryScopeAllParts	    0
-seQueryScopeHiddenParts	    2
-seQueryScopeSelectedParts	3
-seQueryScopeShownParts	    1
-
-
-/// Query Property Constants
-
-Member	                          Value
-seQueryPropertyAuthor	            3
-seQueryPropertyCategory	            6
-seQueryPropertyComments	            8
-seQueryPropertyCompany	            5
-seQueryPropertyCustom	            15
-seQueryPropertyCustomOccurrence	    16
-seQueryPropertyDocumentNumber	    9
-seQueryPropertyKeywords	            7
-seQueryPropertyManager	            4
-seQueryPropertyMaterial	            12
-seQueryPropertyName	                0
-seQueryPropertyProject	            11
-seQueryPropertyReference	        14
-seQueryPropertyRevisionNumber	    10
-seQueryPropertyStatus	            13
-seQueryPropertySubject	            2
-seQueryPropertyTitle	            1
-
-
-/// QueryConditionConstants Enumeration
-
-seQueryConditionContains	0
-seQueryConditionIs	        1
-seQueryConditionIsNot	    2
-"""
 
 import sys
 import clr
@@ -52,139 +12,214 @@ import System
 import System.Collections
 import System.Runtime.InteropServices as SRI
 from System import Console
+
 # import SolidEdgeAssembly as SEAssembly
-from SolidEdgeAssembly.QueryScopeConstants import  seQueryScopeAllParts
+
 from SolidEdgeAssembly.QueryPropertyConstants import seQueryPropertyCategory
 from SolidEdgeAssembly.QueryPropertyConstants import seQueryPropertyCustom
 from SolidEdgeAssembly.QueryPropertyConstants import seQueryPropertyReference
 from SolidEdgeAssembly.QueryConditionConstants import seQueryConditionContains
 from SolidEdgeAssembly.QueryConditionConstants import seQueryConditionIsNot
+from SolidEdgeAssembly.QueryConditionConstants import seQueryConditionIs
 
-def create_query(queries, query_name, criterias=None, sub=True):
-    """Create a query in select tools panel.
-    """
-
-    # Add the query here:
-    query = queries.Add(query_name)
-    query.Scope = seQueryScopeAllParts
-    query.SearchSubassemblies = sub
-
-    # loop throught the criterias
-    for criteria in criterias:
-        query.AddCriteria(
-            criteria[0],
-            criteria[1],
-            criteria[2],
-            criteria[3],
-    )
-    print("[QUERY]: Created: {0:.<25}{1:.>25}".format(query_name, query.MatchesCount.ToString()))
+from Queries import CriteriaProperties
+from helper import create_query
 
 
-def main():
-    application = SRI.Marshal.GetActiveObject("SolidEdge.Application")
-    asm = application.ActiveDocument
+def application():
+    return SRI.Marshal.GetActiveObject("SolidEdge.Application")
+
+
+def active_document(application):
+    return application.ActiveDocument
+
+
+def create_various_queries(asm):
+    # asm = application.ActiveDocument
     print("part: %s\n" % asm.Name)
     assert asm.Type == 3, "This macro only works on .asm"
 
-    fasteners = False
+    # HARDWARE QUERIES
+    # ==================
+
+    # hardware object is created only once because it is used in all the following queries
+    hardware = CriteriaProperties(
+        seQueryPropertyCategory, "Category", seQueryConditionContains, "HARDWARE"
+    )
 
     # Hardware [PLATED.ZINC]
+    zinc = CriteriaProperties(
+        seQueryPropertyCustom, "DSC_A", seQueryConditionContains, "ZINC PLATED"
+    )
     create_query(
-            asm.Queries,
-            "Hardware [PLATED.ZINC]",
-            [
-                (seQueryPropertyCategory, "Category", seQueryConditionContains ,"HARDWARE"),
-                (seQueryPropertyCustom, "DSC_A", seQueryConditionContains , "ZINC PLATED"),
-            ]
+        asm.Queries, "Hardware [PLATED.ZINC]", [hardware.criterias, zinc.criterias]
     )
 
     # Hardware [SS]
-    create_query(
-            asm.Queries,
-            "Hardware [SS]",
-            [
-                (seQueryPropertyCategory, "Category", seQueryConditionContains ,"HARDWARE"),
-                (seQueryPropertyCustom, "DSC_F", seQueryConditionContains , "SS.3"),
-            ]
+    ss = CriteriaProperties(
+        seQueryPropertyCustom, "DSC_F", seQueryConditionContains, "SS.3"
     )
+    create_query(asm.Queries, "Hardware [SS]", [hardware.criterias, zinc.criterias])
+
     # Hardware [SS.304]
-    create_query(
-            asm.Queries,
-            "Hardware [SS.304]",
-            [
-                (seQueryPropertyCategory, "Category", seQueryConditionContains ,"HARDWARE"),
-                (seQueryPropertyCustom, "DSC_F", seQueryConditionContains , "[SS.304]"),
-            ]
+    ss304 = CriteriaProperties(
+        seQueryPropertyCustom, "DSC_F", seQueryConditionContains, "[SS.304]"
     )
+    create_query(
+        asm.Queries, "Hardware [SS.304]", [hardware.criterias, ss304.criterias]
+    )
+
     # Hardware [SS.316]
-    create_query(
-            asm.Queries,
-            "Hardware [SS.316]",
-            [
-                (seQueryPropertyCategory, "Category", seQueryConditionContains ,"HARDWARE"),
-                (seQueryPropertyCustom, "DSC_F", seQueryConditionContains , "[SS.316]"),
-            ]
+    ss316 = CriteriaProperties(
+        seQueryPropertyCustom, "DSC_F", seQueryConditionContains, "[SS.316]"
     )
+    create_query(
+        asm.Queries, "Hardware [SS.316]", [hardware.criterias, ss316.criterias]
+    )
+
     # "Hardware INCH"
-    create_query(
-            asm.Queries,
-            "Hardware INCH",
-            [
-                (seQueryPropertyCategory, "Category", seQueryConditionContains ,"HARDWARE"),
-                (seQueryPropertyCustom, "JDEPRP1", seQueryConditionIsNot , "Metric Fastener"),
-            ]
+    inch = CriteriaProperties(
+        seQueryPropertyCustom, "JDEPRP1", seQueryConditionIsNot, "Metric Fastener"
     )
+    create_query(asm.Queries, "Hardware INCH", [hardware.criterias, inch.criterias])
+
     # "Hardware METRIC"
+    metric = CriteriaProperties(
+        seQueryPropertyCustom, "JDEPRP1", seQueryConditionContains, "Metric Fastener"
+    )
+    create_query(asm.Queries, "Hardware METRIC", [hardware.criterias, metric.criterias])
+
+    # REFERENCE QUERIES
+    # ==================
+
+    # "Reference [Excluded from Assembly Reports]"
+    ref_reports = CriteriaProperties(
+        seQueryPropertyReference,
+        "Reference",
+        seQueryConditionIs,
+        "Excluded from Assembly Reports",
+    )
     create_query(
-            asm.Queries,
-            "Hardware METRIC",
-            [
-                (seQueryPropertyCategory, "Category", seQueryConditionContains ,"HARDWARE"),
-                (seQueryPropertyCustom, "JDEPRP1", seQueryConditionContains , "Metric Fastener"),
-            ]
+        asm.Queries,
+        "Reference [Excluded from Assembly Reports]",
+        [ref_reports.criterias],
     )
 
-
-
-    # "Reference"
-    create_query(
-            asm.Queries,
-            "Reference",
-            [
-                (seQueryPropertyReference, "Reference", seQueryConditionContains ,"HARDWARE"),
-            ]
+    # "Reference [Not displayed in drawing views]"
+    not_display_drawings = CriteriaProperties(
+        seQueryPropertyReference,
+        "Reference",
+        seQueryConditionIs,
+        "Not displayed in drawing views",
     )
-    # "Reference"
     create_query(
-            asm.Queries,
-            "Reference",
-            [
-                (seQueryPropertyReference, "Reference", seQueryConditionContains ,"HARDWARE"),
-            ]
-    )
-    # "Reference"
-    create_query(
-            asm.Queries,
-            "Reference",
-            [
-                (seQueryPropertyReference, "Reference", seQueryConditionContains ,"HARDWARE"),
-            ]
+        asm.Queries,
+        "Reference [Not displayed in drawing views]",
+        [not_display_drawings.criterias],
     )
 
+    # "Reference [Displayed as Reference in Drawing Views]"
+    ref_drawings = CriteriaProperties(
+        seQueryPropertyReference,
+        "Reference",
+        seQueryConditionIs,
+        "Displayed as Reference in Drawing Views",
+    )
+    create_query(
+        asm.Queries,
+        "Reference [Displayed as Reference in Drawing Views]",
+        [ref_drawings.criterias],
+    )
+
+    # MATERIAL QUERIES
+    # ==================
+
+    # "STEEL PLATE"
+    steel_plate = CriteriaProperties(
+        seQueryPropertyCustom, "JDEDSC1_A", seQueryConditionContains, "STEEL PLATE"
+    )
+    create_query(asm.Queries, "Material [STEEL PLATE]", [steel_plate.criterias])
+
+    # "SS.PLATE"
+    ss_plate = CriteriaProperties(
+        seQueryPropertyCustom, "JDEDSC1_A", seQueryConditionContains, "SS.PLATE"
+    )
+    create_query(asm.Queries, "Material [SS.PLATE]", [ss_plate.criterias])
+
+    # "ALU"
+    alu_plate = CriteriaProperties(
+        seQueryPropertyCustom, "JDEDSC1_A", seQueryConditionContains, "AL.PLATE"
+    )
+    create_query(asm.Queries, "Material [AL.PLATE]", [alu_plate.criterias])
 
 
-# TODO: Add reference object
-# TODO: add non released items
+def stop():
+    sys.exit()
 
 
-def confirmation(func):
-    response = raw_input("""Create fasteners/reference queries? (Press y/[Y] to proceed.)""")
-    if response.lower() not in ["y", "yes"]:
+def remove_all_queries(assembly):
+    print("queries number: %s" % assembly.Queries.Count)
+    # Remove query in the collection of queries
+    created_queries = [
+        "Hardware [PLATED.ZINC]",
+        "Hardware [SS]",
+        "Hardware [SS.304]",
+        "Hardware [SS.316]",
+        "Hardware INCH",
+        "Hardware METRIC",
+        "Reference [Excluded from Assembly Reports]",
+        "Reference [Not displayed in drawing views]",
+        "Reference [Displayed as Reference in Drawing Views]",
+        "Material [STEEL PLATE]",
+        "Material [SS.PLATE]",
+        "Material [AL.PLATE]",
+    ]
+
+    for query in created_queries:
+        assembly.Queries.Remove(query)
+        print("[DELETED] %s " % query)
+
+
+def would_do_like_to_create_or_removeall_queries():
+    response = raw_input(
+        """Create queries[Q] or delete[D] all queries? (Press [Q] or [D] to proceed...)"""
+    ).lower()
+    choice = {"q": create_various_queries, "d": remove_all_queries}
+    return choice.get(response)
+
+
+def user_confirmation_to_continue():
+    response = raw_input(
+        """Create fasteners/reference queries? (Press y/[Y] to proceed.)"""
+    )
+    if response.lower() in ["y", "yes"]:
+        pass
+    else:
         print("Process canceled")
         sys.exit()
-    else:
-        func()
+
+
+def main():
+    try:
+        user_confirmation_to_continue()
+        answer = would_do_like_to_create_or_removeall_queries()
+        app = application()
+        assembly = active_document(app)
+        if answer is create_various_queries:
+            create_various_queries(assembly)
+        elif answer is remove_all_queries:
+            remove_all_queries(assembly)
+        else:
+            pass
+
+    except Exception as ex:
+        print(ex)
+
+    finally:
+        raw_input("\nPress any key to exit...")
+        stop()
+
 
 
 if __name__ == "__main__":
-    confirmation(main)
+    main()
